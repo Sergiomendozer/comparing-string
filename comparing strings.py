b94ea284be434e5a4b5e2d22e026b8df873aa566
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-10 21:01:16 -0800

    function if statment and while loop works well

--- comparing strings.py
+++ comparing strings.py
@@ -2,18 +2,40 @@ END = '\033[0m'
 Red="\033[0;31m" 
 Green="\033[0;32m"       
 
-def compare(string_1,string_2):
+def compare_characters(i,string_1,string_2):
     length_string_1 = len(string_1)
     length_string_2 = len(string_2)
+    if length_string_2 > length_string_1:
+        # while i <= length_string_2:
+        print("two")
+    elif length_string_1 > length_string_2:
+        while i <= length_string_1:
+            if string_1[i] == string_2[i]:
+                i =i + 1
+                return compare_characters(i,string_1,string_2)
+            elif string_1[i] != string_2[i]:
+                print(string_1[:i-1] + Red +string_1[i:] +END)
+                print(string_2[:i-1] + Red +string_2[i:] +END)
+                i = length_string_1
+            else:
+                print (Green + "string are the same"+ END)
+    else:
+        print("3")
+
+def compare_length(string_1,string_2):
+    length_string_1 = len(string_1)
+    length_string_2 = len(string_2)
+    i = 1
     if length_string_2 == length_string_1:
         print(Green + "Strings are the same length" + END)
+        compare_characters(i,string_1,string_2)
     else:
-        print ("string 1 length is " + length_string_1)
-        print ("string 2 length is " + length_string_2)
+        length_string_1 = str(length_string_1)
+        length_string_2 = str(length_string_2)
+        print ("String 1 length is " + length_string_1)
+        print ("String 2 length is " + length_string_2)
         print(Red + "Strings are not the same length" + END)
-        print(string_1)
-        print (string_2)
-
+        compare_characters(i,string_1,string_2)
 
 
 
@@ -22,6 +44,6 @@ def compare(string_1,string_2):
 # string_1 = (str(input("Enter first string:\n")))
 # string_2 = (str(input("Enter second string:\n")))
 string_1 = "string_1dc"
-string_2 = "string_2cd"
+string_2 = "string_2d"
 
-compare(string_1,string_2)
+compare_length(string_1,string_2)
