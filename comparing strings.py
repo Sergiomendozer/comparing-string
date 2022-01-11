cae59df18b88d4a5796748638de11281450514ec
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-10 21:58:11 -0800

    comments added and cleaner code

--- comparing strings.py
+++ comparing strings.py
@@ -1,7 +1,7 @@
 END = '\033[0m'
 Red="\033[0;31m" 
 Green="\033[0;32m"       
-
+#compares letter by letter if charcaters are the same
 def compare_characters(i,string_1,string_2):
     length_string_1 = len(string_1)
     length_string_2 = len(string_2)
@@ -13,7 +13,7 @@ def compare_characters(i,string_1,string_2):
             elif string_1[i] != string_2[i]:
                 print(string_1[:i-1] + Red +string_1[i:] +END)
                 print(string_2[:i-1] + Red +string_2[i:] +END)
-                i = length_string_2 + 5
+                i = length_string_2 *5
     elif length_string_1 > length_string_2:
         while i <= length_string_1:
             if string_1[i] == string_2[i]:
@@ -22,10 +22,10 @@ def compare_characters(i,string_1,string_2):
             elif string_1[i] != string_2[i]:
                 print(string_1[:i-1] + Red +string_1[i:] +END)
                 print(string_2[:i-1] + Red +string_2[i:] +END)
-                i = length_string_1 + 5
+                i = length_string_1 *5
     else:
         print (Green + "string are the same"+ END)
-
+#function compares length of string to give user a clue if they are the same or not
 def compare_length(string_1,string_2):
     length_string_1 = len(string_1)
     length_string_2 = len(string_2)
@@ -41,9 +41,9 @@ def compare_length(string_1,string_2):
         print(Red + "Strings are not the same length" + END)
         compare_characters(i,string_1,string_2)
 
+# where input will be entered
 # string_1 = (str(input("Enter first string:\n")))
 # string_2 = (str(input("Enter second string:\n")))
 string_1 = "string_1dc"
-string_2 = "string_2dc"
-
+string_2 = "string_2d"
 compare_length(string_1,string_2)
